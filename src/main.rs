b431c56de89183dbad0b696601278e051bcf5648
Oery <oery.dev@gmail.com>
2025-02-13 00:14:04 +0100

    Update example

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,20 @@ async fn main() -> anyhow::Result<()> {
     let mut bot = BotBuilder::new();
 
     bot.on_chat(|ctx: &Context<Chat>| {
-        if ctx.payload.message.contains("ping") {
-            ctx.bot.chat("pong!");
+        if ctx.payload.message.contains("attack") {
+            for entity in ctx.bot.world.entities.iter() {
+                ctx.bot.attack_entity(entity.id);
+            }
+        }
+
+        if ctx.payload.message.contains("entity_id") {
+            ctx.bot.chat(&format!("Entity ID: {}", ctx.bot.entity_id()));
+        }
+    });
+
+    bot.on_tick(|ctx: &Context<()>| {
+        for entity in ctx.bot.world.entities.iter() {
+            ctx.bot.attack_entity(entity.id);
         }
     });
 
