66c30882c705b5a58580fa02c1494e9f147f112b
Oery <oery.dev@gmail.com>
2025-05-24 14:56:51 +0200

    add crud methods for scoreboards

--- src/scores.rs
+++ src/scores.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::rc::Rc;
 
+use gami_mc_protocol::packets::play::server::UpdateTeam;
 use gami_mc_protocol::{
     packets::play::server::{CreateTeam, FriendlyFire, ScoreboardKind},
     registry::TextColor,
@@ -14,6 +15,35 @@ pub struct Scores {
     pub player_list: Option<Rc<str>>,
     pub sidebar: Option<Rc<str>>,
     pub team_sidebar: Option<Rc<str>>,
+}
+
+impl Scores {
+    pub fn get_objective(&mut self, name: impl Into<Rc<str>>) -> Option<&mut Objective> {
+        self.objectives.get_mut::<Rc<str>>(&name.into())
+    }
+
+    pub fn create_team(&mut self, name: impl Into<Rc<str>>, team: &CreateTeam) {
+        self.teams.insert(name.into(), Team::from(team));
+    }
+
+    pub fn remove_team(&mut self, name: impl Into<Rc<str>>) {
+        self.teams.remove(&name.into());
+    }
+
+    pub fn get_team(&mut self, name: impl Into<Rc<str>>) -> Option<&mut Team> {
+        self.teams.get_mut(&name.into())
+    }
+
+    pub fn update_team(&mut self, name: impl Into<Rc<str>>, team: &UpdateTeam) {
+        if let Some(prev_team) = self.teams.get_mut(&name.into()) {
+            prev_team.display_name = team.display_name.clone();
+            prev_team.prefix = team.prefix.clone();
+            prev_team.suffix = team.suffix.clone();
+            prev_team.friendly_fire = team.friendly_fire;
+            prev_team.nametag_visibility = team.nametag_visibility.clone();
+            prev_team.color = team.color;
+        }
+    }
 }
 
 #[derive(Default, Debug)]
@@ -41,6 +71,16 @@ pub struct Team {
     pub friendly_fire: FriendlyFire,
     pub nametag_visibility: String,
     pub color: TextColor,
+}
+
+impl Team {
+    pub fn add_players(&mut self, players: &[String]) {
+        self.players.extend(players.iter().cloned());
+    }
+
+    pub fn remove_players(&mut self, players: &[String]) {
+        self.players.retain(|player| !players.contains(player));
+    }
 }
 
 impl From<&CreateTeam> for Team {
